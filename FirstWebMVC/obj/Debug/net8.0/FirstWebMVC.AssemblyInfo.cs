ae8d65a809bf2db53ccf0e022c4fb28cb83b4607
“luongThien18092002 <thien18092002@gmail.com>
2024-01-25 09:08:19 +0700

    PTPMQL

--- FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6382ebb890094f54811bf04041e9dd219e6e2a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8aaf8a4ae8cebef35ea6113a585e6d5a857e92d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
