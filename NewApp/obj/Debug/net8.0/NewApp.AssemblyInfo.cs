d2054d288cfca1c6b431f6e8de3665f018618def
“luongThien18092002 <thien18092002@gmail.com>
2024-02-02 15:38:53 +0700

    PTPMQL

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb8d7ff1be97412fde5409b35c5eeb5a9ecba07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09e14ff3647cd9c5809b01a815f830a4995aa9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
