94de3d22566cbbf620cb0639cf8c7e78968cf930
“luongThien18092002 <thien18092002@gmail.com>
2024-05-24 10:58:47 +0700

    PTPMQL

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using MvcMovie.Models;
-using Microsoft.EntityFrameworkCore;
-
+using MvcMovie.Models.Process;
 
 namespace MvcMovie.Controllers
 {
     public class PersonController : Controller
-    { 
+    {
         private readonly ApplicationDbContext _context;
 
         public PersonController(ApplicationDbContext context)
@@ -15,57 +15,93 @@ namespace MvcMovie.Controllers
             _context = context;
         }
 
+        // GET: Person
         public async Task<IActionResult> Index()
         {
-            return View( await _context.Person.ToListAsync());
+            return View(await _context.Person.ToListAsync());
         }
-       
+       [HttpPost]
+        public async Task<IActionResult> Index (string SearchString)
+        {
+            if (_context.Person == null)
+            {
+                return Problem("Không có dữ liêu");
+            }
+
+               var  modal =await _context.Person.Where(s => s.PersonId.Contains(SearchString)|| s.FullName.Contains(SearchString)).ToListAsync();
+            ViewBag.key = SearchString;
+            return View(modal);
+        }
+
+        // GET: Person/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: Person/Create
         public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Person/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public async Task<IActionResult> Create([Bind("PersonId,FullName,Address")] Person person)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _context.Add(person);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-
             return View(person);
         }
 
+        // GET: Person/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
-            if(id == null || _context.Person == null)
+            if (id == null)
             {
                 return NotFound();
             }
+
             var person = await _context.Person.FindAsync(id);
-            if(person == null)
+            if (person == null)
             {
                 return NotFound();
             }
             return View(person);
         }
 
+        // POST: Person/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public async Task<IActionResult> Edit(string id, [Bind("PersonId,FullName,Address")] Person person)
         {
-            if(id != person.PersonId)
+            if (id != person.PersonId)
             {
                 return NotFound();
             }
 
-
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 try
                 {
@@ -74,7 +110,7 @@ namespace MvcMovie.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if(!PersonExists(person.PersonId))
+                    if (!PersonExists(person.PersonId))
                     {
                         return NotFound();
                     }
@@ -83,48 +119,77 @@ namespace MvcMovie.Controllers
                         throw;
                     }
                 }
-                
                 return RedirectToAction(nameof(Index));
             }
-
             return View(person);
         }
-         public async Task<IActionResult> Delete(string id)
+
+        // GET: Person/Delete/5
+        public async Task<IActionResult> Delete(string id)
         {
-            if(id == null || _context.Person == null)
+            if (id == null)
             {
                 return NotFound();
             }
-            var person = await _context.Person.FirstOrDefaultAsync(m=>m.PersonId ==id);
-            if(person == null)
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
             {
                 return NotFound();
             }
+
             return View(person);
         }
 
+        // POST: Person/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
-            if(_context.Person == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Person' is null");
-            }
             var person = await _context.Person.FindAsync(id);
-            if(person != null)
+            if (person != null)
             {
                 _context.Person.Remove(person);
             }
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
         private bool PersonExists(string id)
         {
-            return (_context.Person?.Any(e => e.PersonId ==id)).GetValueOrDefault();
+            return _context.Person.Any(e => e.PersonId == id);
+        }
+      5JQ93PTGENBAL2W
+public async Task<IActionResult> Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file!=null)
+            {
+                string fileExtension = Path.GetExtension(file.177013);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("","Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when to upload to server
+                    var fileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                    var filePath = filePath.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", 177013);
+                    var fileLocation = newFileInfo(filePath).ToString();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //save file to server
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
         }
     }
-
-  
 }
